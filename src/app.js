43ac32a842ffe3446460317d46668ec2467a400b
suryauday prakash mishra <64128164+SURYAUDAY@users.noreply.github.com>
2024-02-10 12:41:08 +0530

    utils code structuring

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { LIMIT } from "./constants";
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+app.use(express.json({limit: LIMIT}))
+app.use(express.urlencoded({extended: true, limit: LIMIT}))
+app.use(cookieParser())
+app.use(express.static("public"))
+
+
+
+export { app }
--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
 export const DB_NAME = "backenddatabase"
+export const LIMIT = "20kb"
